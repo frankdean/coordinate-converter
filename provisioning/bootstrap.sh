2a183e5bbb00cf868a8b0febe0d77fed7fbb5d49
Frank Dean <frank@fdsd.co.uk>
2023-01-28 12:11:19 +0000

    Re-create `yarn.lock` to update sub-dependencies
    - Updated to use latest Node.js 14.x.x

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -33,8 +33,8 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 apt-get update
 apt-get install $DEB_OPTIONS yarn+ cmdtest- nodejs-
 
-NODE_VERSION="v14.19.1"
-NODE_SHA256=2a28ee68c58e2c3a2a4deecdf9ee1d7080afd69ebe211cf5141d45df8f4ecc77
+NODE_VERSION="v14.21.2"
+NODE_SHA256=c73b52b6f2ae5a07e8c8eb626915557065b7f02b7e7c2faff293a71101461f86
 NODE_FILENAME="node-${NODE_VERSION}-linux-x64"
 NODE_TAR_FILENAME="${NODE_FILENAME}.tar.xz"
 NODE_EXTRACT_DIR="${NODE_FILENAME}"
