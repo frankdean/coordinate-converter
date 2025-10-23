6a3afc1a7c6caa64eb7f04e947c0e32294ef084e
Frank Dean <frank@fdsd.co.uk>
2025-10-23 19:50:37 +0100

    Updated provisioning scripts for both Vagrant and directly with QEMU

--- provisioning/bootconfig.sh
+++ provisioning/bootconfig.sh
@@ -3,26 +3,38 @@
 # Uncomment the following to debug the script
 #set -x
 
+# This is the folder where the source code is available in the VM
+COORD_SOURCE=${COORD_SOURCE:-/vagrant}
+# The user and group name for building Trip Server
+USERNAME=${USERNAME:-vagrant}
+GROUPNAME=${GROUPNAME:-${USERNAME}}
+
 function setup_nginx
 {
     if [ ! -e /etc/nginx/sites-available/convert-coords ]; then
-	cp /vagrant/provisioning/nginx/sites-available/convert-coords /etc/nginx/sites-available
+	cp ${COORD_SOURCE}/provisioning/nginx/sites-available/convert-coords /etc/nginx/sites-available
+	if [ "$USERNAME" != "vagrant" ]; then
+	    grep -E '^\s+root /vagrant/dist;' /etc/nginx/sites-available/convert-coords
+	    if [ $? -eq 0 ]; then
+		sed -i "s~/vagrant/dist~/home/${USERNAME}/Projects/convert-coord/dist~" /etc/nginx/sites-available/convert-coords
+	    fi
+	fi
 	if [ -e /etc/nginx/sites-enabled/default ]; then
 	    rm -f /etc/nginx/sites-enabled/default
 	fi
 	ln -fs /etc/nginx/sites-available/convert-coords /etc/nginx/sites-enabled/convert-coords
-	systemctl restart nginx
+	systemctl reload nginx
     fi
 }
 
 setup_nginx
-su - vagrant -c 'cd /vagrant && npm install'
+su - ${USERNAME} -c "cd ${COORD_SOURCE} && npm install"
 
-if [ ! -f /home/vagrant/dist/index.html ]; then
-    su - vagrant -c 'cd /vagrant && npm run build'
+if [ ! -f /home/${USERNAME}/dist/index.html ]; then
+    su - ${USERNAME} -c "cd ${COORD_SOURCE} && npm run build"
 fi
 
-egrep '^export\s+EDITOR' /home/vagrant/.profile >/dev/null 2>&1
+egrep '^export\s+EDITOR' /home/${USERNAME}/.profile >/dev/null 2>&1
 if [ $? -ne 0 ]; then
-	echo "export EDITOR=/usr/bin/vi" >>/home/vagrant/.profile
+	echo "export EDITOR=/usr/bin/vi" >>/home/${USERNAME}/.profile
 fi
--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -3,19 +3,41 @@
 # Uncomment the following to debug the script
 #set -x
 
-NODE_VERSION="v20.12.2"
-NODE_SHA256=f8f9b6877778ed2d5f920a5bd853f0f8a8be1c42f6d448c763a95625cbbb4b0d
-NODE_FILENAME="node-${NODE_VERSION}-linux-x64"
+# This is the folder where the source code is available in the VM
+COORD_SOURCE=${COORD_SOURCE:-/vagrant}
+# The user and group name for building Trip Server
+USERNAME=${USERNAME:-vagrant}
+GROUPNAME=${GROUPNAME:-${USERNAME}}
+PROCESSOR="$(uname -p)"
+ARCH="$(arch)"
+echo $ARCH
+
+# https://nodejs.org/dist/latest-v22.x/SHASUMS256.txt
+NODE_VERSION="v22.21.0"
+if [ "$PROCESSOR" != "unknown" ]; then
+    NODE_ARCH=x64
+    NODE_SHA256=262b84b02f7e2bc017d4bdb81fec85ca0d6190a5cd0781d2d6e84317c08871f8
+elif [ "$ARCH" == "aarch64" ]; then
+    NODE_ARCH=arm64
+    NODE_SHA256=5180e74f2cdea6142548bd827249da32c0e86ba2581d6241e2f2761f957b9ed1
+else
+    NODE_SHA256=262b84b02f7e2bc017d4bdb81fec85ca0d6190a5cd0781d2d6e84317c08871f8
+    NODE_ARCH=x64
+fi
+NODE_FILENAME="node-${NODE_VERSION}-linux-${NODE_ARCH}"
 NODE_TAR_FILENAME="${NODE_FILENAME}.tar.gz"
 NODE_EXTRACT_DIR="${NODE_FILENAME}"
 NODE_DOWNLOAD_URL="https://nodejs.org/dist/${NODE_VERSION}/${NODE_TAR_FILENAME}"
-DOWNLOAD_CACHE_DIR="/vagrant/provisioning/downloads"
+DOWNLOAD_CACHE_DIR="${COORD_SOURCE}/provisioning/downloads"
 
-SU_CMD="su vagrant -c"
+echo "Source folder: ${COORD_SOURCE}"
+echo "Username: ${USERNAME}"
+echo "Groupname: ${USERNAME}:${GROUPNAME}"
+
+SU_CMD="su $USERNAME -c"
 
 function install_nodejs
 {
-    # https://github.com/nodejs/help/wiki/Installation
     if [ ! -x "/usr/local/lib/nodejs/${NODE_EXTRACT_DIR}/bin/node" ]; then
 	# Download the tarball distribution if it does not already exist
 	if [ ! -e "${DOWNLOAD_CACHE_DIR}/${NODE_TAR_FILENAME}" ]; then
@@ -40,22 +62,23 @@ function install_nodejs
 		rm -f /usr/local/lib/nodejs/node-current
 	    fi
 	    ln -sf "/usr/local/lib/nodejs/${NODE_EXTRACT_DIR}" /usr/local/lib/nodejs/node-current
-	    if [ -e /vagrant/node_modules ]; then
-		cd /vagrant
+	    if [ -e ${COORD_SOURCE}/node_modules ]; then
+		cd ${COORD_SOURCE}
 		rm -rf node_modules
 	    fi
 	fi
     fi
-    egrep '^export\s+PATH.*nodejs' /home/vagrant/.profile >/dev/null 2>&1
+    egrep '^export\s+PATH.*nodejs' /home/${USERNAME}/.profile >/dev/null 2>&1
     if [ $? -ne 0 ]; then
-	echo "export PATH=/usr/local/lib/nodejs/node-current/bin:$PATH" >>/home/vagrant/.profile
+	echo "export PATH=/usr/local/lib/nodejs/node-current/bin:$PATH" >>/home/${USERNAME}/.profile
     fi
-    if [ -x /usr/local/lib/nodejs/node-current/bin/node ] && \
-	   [ ! -x /usr/local/lib/nodejs/node-current/lib/node_modules/yarn/bin/yarn ]; then
-	PATH="/usr/local/lib/nodejs/node-current/bin:$PATH" \
-	    /usr/local/lib/nodejs/node-current/lib/node_modules/npm/bin/npm-cli.js \
-	    install -g yarn
-    fi
+    # if [ -x /usr/local/lib/nodejs/node-current/bin/node ] && \
+    # 	   [ ! -x /usr/local/lib/nodejs/node-current/lib/node_modules/yarn/bin/yarn ]; then
+    # 	PATH="/usr/local/lib/nodejs/node-current/bin:$PATH" \
+    # 	    /usr/local/lib/nodejs/node-current/lib/node_modules/npm/bin/npm-cli.js \
+    # 	    install -g yarn
+    # fi
+
     # Upgrade npm to latest available version
     if [ -x /usr/local/lib/nodejs/node-current/bin/node ]; then
 	PATH="/usr/local/lib/nodejs/node-current/bin:$PATH" \
@@ -94,6 +117,8 @@ if [ -f /etc/debian_version ]; then
 
     apt-get install $DEB_OPTIONS apt-transport-https
     apt-get install $DEB_OPTIONS curl git vim screen nginx-light
+
+    # apt-get install $DEB_OPTIONS nodejs npm
 fi
 
 install_nodejs
